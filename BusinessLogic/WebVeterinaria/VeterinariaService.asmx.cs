ece25a5bcf44dc9741a41c1e16f245bb1fc14346
Ivan Lopez <Ivan Lopez@DESKTOP-1DGLCJI>
2017-02-12 20:01:33 +0100

    Se agrego el metodo de mostrar y agregar mascotas

--- BusinessLogic/WebVeterinaria/VeterinariaService.asmx.cs
+++ BusinessLogic/WebVeterinaria/VeterinariaService.asmx.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Services;
+using MySql.Data.MySqlClient;
+using BusinessEntities;
+using System.Data;
+using System.Configuration;
+using MySql.Data;
+
+//the stage-Avenged 
+
+
 
 namespace WebVeterinaria
 {
@@ -16,13 +25,53 @@ namespace WebVeterinaria
     // [System.Web.Script.Services.ScriptService]
     public class VeterinariaService : System.Web.Services.WebService
     {
+        
+        
 
+        
         [WebMethod]
-        public string HelloWorld()
+        public void InsertMascota(String Nombre, string Raza, string Especie, float Peso, String Genero, int Edad)
         {
-            return "Hola a todos";
-        }
 
+            string connStr = "server=localhost;user id=root;database=veterinariayei";
+            using (MySqlConnection con = new MySqlConnection(connStr))
+            {
+                using (MySqlCommand cmd = new MySqlCommand("InsertMascota", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.Add("@Nombre", MySqlDbType.VarChar, 50).Value = Nombre;
+                    cmd.Parameters.Add("@Raza", MySqlDbType.VarChar, 50).Value = Raza;
+                    cmd.Parameters.Add("@Especie", MySqlDbType.VarChar, 65532).Value = Especie;
+                    cmd.Parameters.Add("@Peso", MySqlDbType.Float).Value = Peso;
+                    cmd.Parameters.Add("@Genero", MySqlDbType.VarChar, 65532).Value = Genero;
+                    cmd.Parameters.Add("@Edad", MySqlDbType.Int16).Value = Edad;
+                    cmd.ExecuteNonQuery();
+
+                }
+            }
+        }
+        [WebMethod]
+        public DataTable MostrarMascotas()
+        {
+            string connStr = "server=localhost;user id=root;database=veterinariayei";
+            using (MySqlConnection con = new MySqlConnection(connStr))
+            {
+                using (MySqlCommand cmd = new MySqlCommand("SELECT * FROM mascotas"))
+                {
+                    using (MySqlDataAdapter sda = new MySqlDataAdapter())
+                    {
+                        cmd.Connection = con;
+                        sda.SelectCommand = cmd;
+                        using (DataTable dt = new DataTable())
+                        {
+                            dt.TableName = "Mascotas";
+                            sda.Fill(dt);
+                            return dt;
+                        }
+                    }
+                }
+            }
+        }
 
     }
 }
